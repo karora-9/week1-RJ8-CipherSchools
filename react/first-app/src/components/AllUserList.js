f5a53ee4ca6056ed4503d3dca6c8fe87da4ae96b
karora-9 <arorakanishka80@gmail.com>
2023-01-27 23:02:46 +0530

    Added Files

--- react/first-app/src/components/AllUserList.js
+++ react/first-app/src/components/AllUserList.js
@@ -1,21 +1,68 @@
-import React from "react";
+import React,{ useState } from "react";
+import Test from  "./Test";
+import UserDetails from "./UserDetails";
+
 
 const AllUserList = () => {
-    let allUsers = [
-        {name: "Kanishka", email: "kanishka@email.com", phoneNumber: "1232399999"},
-        {name: "Gauri", email: "gauri@email.com", phoneNumber: "1223567777"},
-        {name: "Ahmad", email: "ahamd@email.com", phoneNumber: "1583445555"},
-
- 
-
+    const [state, setState] = useState({
+        name: "Akb",
+    });
+    // state = {
+    //     name: "Priyanshu Saxena",
+    // };
+    //allUsers: [
+        let allUsers =[
+            {name: "Kanishka", email: "kanishka@email.com", phoneNumber: "1232399999"},
+            {name: "Gauri", email: "gauri@email.com", phoneNumber: "1223567777"},
+            {name: "Ahmad", email: "ahamd@email.com", phoneNumber: "1583445555"},
     ];
+
+
+    // let array = [
+    //     <UserDetails key={0} user={allUsers[0]} />,
+    //     <UserDetails key={1} user={allUsers[1]} />,
+    //     <UserDetails key={2} user={allUsers[2]} />,
+
+    // ];
+    // return (
+    //     <div>
+    //     <userDetails user={allUsers[0]} />
+    //     <userDetails user={allUsers[1]} />
+    //     <userDetails user={allUsers[2]} />
+    //     </div>
+    // )
+
+    // render() {
+    //     setTimeout(() => {
+            
+    //      (() => {
+    //         this.setState({name: "Abhay Raj Gupta" });
+    //     }, 15000);
+
     return (
         <div>
-        <userDetails user={allUsers[0]} />
-        <userDetails user={allUsers[1]} />
-        <userDetails user={allUsers[2]} />
+            <Test name = {state.name} />
+            {allUsers.map((user, index) => (
+                {/* {this.allusers.map((user, index) => ( */}
+                <UserDetails key={index} user={user} />
+            ))}
         </div>
-    )
-};
+    );
+ };
+       
+//         return (
+//             <div>
+//                 <h1>The name is {this.state.name}</h1>
+//                 {this.allUsers.map((user, index) => (
+//                     <UserDetails key={index} user={user} />
+//                 ))}
+//             </div>
+//         );
+//     }
+// }
+
+// const AllUserList = () => {
+//     let [state, setState] = useState( {name: "Priyanshu Saxena"});
+// };
 
 export default AllUserList;
--- react/first-app/src/components/UserDetails.js
+++ react/first-app/src/components/UserDetails.js
@@ -1,6 +1,8 @@
-import React from "react";
-
+import React, { Component } from "react";
+import React,{components}
 const userDetails = ({user = {}}) => {
+    const userDetails = (props) => {
+        let user = props.user;
     return (
         <div>
         <h3>{user.name}</h3>
@@ -12,5 +14,26 @@ const userDetails = ({user = {}}) => {
         </div>
         );
     };
+
+    // class UserDetails extends Component {
+    //     render() {
+    //         this.props.users ={
+    //             name: "Alex",
+    //             email: "alex@email.com",
+    //             phoneNumber: 123456,
+    //         };
+    //         return (
+        
+    //             <div>
+    //             <h3>{user.name}</h3>
+    //                   <h5>
+    //                       Email: {this.user.email}
+    //                       <br/>
+    //                       Phone Number: {this.user.phoneNumber}
+    //                   </h5>
+    //             </div>
+    //         );
+    //     }
+    // }
 
     export default userDetails;
